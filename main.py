2c7107ca808775e9fcbbdfd021028f15a3d16f19
kmusante <kenm@eurekapayments.com>
2016-11-23 08:21:53 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ import random
 import hashlib
 import hmac
 
-
-from google.appengine.ext import ndb
-
 from google.appengine.ext import db #all the above needed in support of program
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -40,7 +37,7 @@ class BlogHandler(webapp2.RequestHandler): #3 things which are copied
         return render_str(template, **params)
 
     def render(self, template, **kw):
-	    self.write(self.render_str(template, **kw))
+        self.write(self.render_str(template, **kw))
 
     def set_secure_cookie(self, name, val):
         cookie_val = make_secure_val(val)
@@ -117,7 +114,7 @@ class User(db.Model):
 
 
 def blog_key(name='default'):  #this is not needed but provides for a parent relationship
-	return db.Key.from_path('blogs', name)#sets stage for multiple blogs
+    return db.Key.from_path('blogs', name)#sets stage for multiple blogs
 
 class Post(db.Model):
     subject=db.StringProperty(required=True) #look up StringProperty, TextProperty, etc
@@ -135,12 +132,12 @@ class BlogFront(BlogHandler):
         deleted_post_id = self.request.get('deleted_post_id')
         posts = db.GqlQuery("select * from Post order by created desc limit 10")
         self.render('front.html', posts=posts, deleted_post_id=deleted_post_id) 
-		#renders result of above query in front.html stored in variable 'posts'
+        #renders result of above query in front.html stored in variable 'posts'
 
 class PostPage(BlogHandler):
     def get(self, post_id): #gets passed in from below but numbers now assigned randomly
         key=db.Key.from_path('Post', int(post_id), parent=blog_key())
-	    #parent only needed because parent was created.  this section is not fully understood
+        #parent only needed because parent was created.  this section is not fully understood
         post=db.get(key)
 
         comments=db.GqlQuery("select * from Comment where post_id = " +post_id+" order by created desc")
@@ -233,42 +230,35 @@ class Like(db.Model):
         user = User.by_id(self.user_id)
         return user.name
 
-'''class DeletePost(BlogHandler):
-    def get(self):
-        if self.user:
-            post_id = self.request.get("post")
-            key = ndb.Key('BlogPost', int(post_id) parent=blog_key())
-            post = key.get()
-            if not post:
-                self.error(404)
-                return
-            self.render("deletepost.html", post=post)
-        else:
-            self.redirect("/login")
-
-    def post(self):
-        if not self.user:
-            return self.redirect("/login")
-        post_id = self.request.get("post")
-        key = ndb.Key('BlogPost', int(post_id) parent=blog_key())
-        post = key.get()
-        if post and post.author.username == self.user.username:
-            key.delete()
-        self.redirect("/blog")'''
-
-class DeletePost(BlogHandler):
-    def get(self):
+'''def get(self, post_id):
         if self.user:
             key = db.Key.from_path('Post', int(post_id), parent=blog_key())
             post = db.get(key)
             if post.user_id == self.user.key().id():
                 post.delete()
-                self.redirect("/?deleted_post_id="+post_id)
+                self.redirect("deletepost.html")
             else:
-                self.redirect("/blog/" + post_id + "?error=You don't have " +
-                              "access to delete this record.")
+                self.redirect('/blog/%s' % str(post_id))
         else:
             self.redirect("/login?error=You need to be logged, in order" +
+                          " to delete your post!!")'''
+
+
+
+class DeletePost(BlogHandler):
+    def get(self, post_id):
+        if self.user:
+            key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+            post = db.get(key)
+            #if post.user.name == self.user.name:
+            #if post.user_id == self.user.key().id():
+            post.delete()
+            #self.redirect(post_id)
+            self.render('deletepost.html')
+            #else:
+            #    self.redirect('/blog/%'  % str(post_id))
+        else:
+            self.redirect("/login error=You need to be logged, in order" +
                           " to delete your post!!")
 
 
@@ -473,12 +463,10 @@ app = webapp2.WSGIApplication([('/', MainPage),
                                ('/blog/newpost', NewPost),
                                ('/blog/deletepost/?', DeletePost),
                                ('/blog/editpost/([0-9]+)', EditPost),
-                               ('/blog/deletecomment/([0-9]+)/([0-9]+)',DeleteComment),
-                               ('/blog/editcomment/([0-9]+)/([0-9]+)', EditComment),
                                ('/signup', Register),
                                ('/login', Login),
                                ('/logout', Logout),
                                ('/unit3/welcome', Unit3Welcome),
                                ('/?', BlogFront)
                                ],
-							debug=True)
+                            debug=True)
